396358afce5b1f9101b56132d520a1371a451448
Daniel Patru <dpatru@gmail.com>
2015-08-20 11:28:10 -0400

    Cleanup.

--- cutlist.hs
+++ cutlist.hs
@@ -1,11 +1,15 @@
+{-# LANGUAGE ViewPatterns #-}
+
 import System.Environment (getArgs)
 import Data.Map.Strict (Map)
-import qualified Data.Map as M ((!),delete,elems,empty,insertWith,lookup,lookupGT,member,minView)
+import qualified Data.Map as M ((!),delete,elems,empty,findWithDefault, insert, insertWith,lookup,lookupGT,member,minView)
 -- import qualified Data.List as L 
-import Data.Sequence ((><), (<|), (|>), Seq)
+import Data.Sequence ((><), (<|), (|>), Seq, viewl, ViewL( EmptyL, (:<) ))
 import qualified Data.Sequence as S (drop,drop,empty,fromList,index,inits,singleton,splitAt,take,zip)
 import Data.List (unfoldr)
 import Control.Exception (assert)
+import Control.Monad (mplus)
+import Data.Foldable (foldlM)
 
 -- Perm == a partial permutation
 type Perm a = (Seq a, Seq a)  -- (permutation so far, rest of characters)
@@ -62,19 +66,72 @@ pop m = do
 popss :: (Ord a) => Perms a -> [Seq a]
 popss m = unfoldr pop m
 
-pops m = map (foldr (:) []) $ popss m
+pops m = map toList $ popss m
 
 isPrefix :: (Eq a) => Seq a -> Seq a -> Bool
 isPrefix x y = (length x <= length y) && (all (uncurry (==)) $ S.zip x y)
 
 -- Remove all the items beginning with x. Expand permutations to
 -- ensure that all permutations are available.
-prunePrefix :: (Ord a) => Seq a -> Perms a -> Perms a
-prunePrefix x m = M.delete x $ removeLonger x m'
+prunePrefix :: (Ord a) => Perms a -> Seq a -> Perms a
+prunePrefix m x = M.delete x $ removeLonger x m'
   where (_, m') = popk m x -- expand m to contain all the x prefixes
         removeLonger x m = case M.lookupGT x m of
            Just (k, v) -> if isPrefix x k then removeLonger x $ M.delete k m else m
            Nothing -> m
+
+type Best a b = Maybe (Seq a, b)
+type Comparator a b = Map (Seq a) b -> Best a b -> Seq a -> (Best a b, [Seq a], Map (Seq a) b)
+
+-- best seq, best score, dynamic prog lookup table 
+
+-- prune :: (Ord a) => Best a b -> Perms a -> Perms a
+-- prune Nothing ps = ps
+-- prune (Just (_, _, prefixes, _)) ps = foldr prunePrefix ps prefixes
+
+-- searchMin :: (Ord a, Ord b) => (Best a b -> Seq a -> Best a b) -> Best a b -> Perms a -> Best a b
+-- searchMin f b ps = (do (p', ps') <- pop ps; searchMin f (f b p') $ prune b ps') `mplus` b
+
+searchMin :: (Ord a, Ord b) => Comparator a b -> Map (Seq a) b -> Best a b -> Perms a -> Best a b
+searchMin f m b ps | null ps = b
+                   | otherwise = do
+  (p', ps') <- pop ps;
+  let (b', ks, m') = f m b p'
+  searchMin f m' b' $ foldl prunePrefix ps' ks
+
+findLongestPrefix :: (Ord a) => Map (Seq a) b -> Seq a -> (Seq a, b)
+findLongestPrefix m x
+  | null x = maybe undefined (\v -> (S.empty, v)) $ M.lookup S.empty m
+  | M.member x m = (x, m M.! x)
+  | otherwise = findLongestPrefix m $ S.take (length x - 1) x
+                  
+oneDComparator :: (Ord a, RealFrac a) => Comparator a a
+oneDComparator m b xs = f m' b [] (longestPrefix, score) (S.drop (length longestPrefix) xs)
+  where m' = M.insertWith (\newVal oldVal -> oldVal) S.empty 0 m
+        (longestPrefix, score) = findLongestPrefix m xs
+        tooBig (_, b) = b > 1
+        f m Nothing e b (viewl -> EmptyL)
+          | tooBig(b) = (Nothing, e, m)
+          | otherwise = (Just b, e, m)
+        f m (Just (a, b)) e (a', b') (viewl -> EmptyL)
+          | b <= b' = (Just (a,b), e ++ [a'], m)
+          | otherwise = (Just (a',b'), e ++ [a], m)
+        f m (Just (a, b)) e (a', b') (viewl -> x :< xs)
+          | x > 1 = (Nothing, e, m')
+          | b'' > b = (Just (a,b), (e ++ [a'']), m')
+          | otherwise = f m' (Just (a,b)) e (a'', b'') xs
+            where a'' = a' |> x
+                  b'' = if ceiling b' == ceiling (b'+x) then b'+x
+                        else (fromIntegral $ ceiling b') + x
+                  m'  = M.insert a'' b'' m
+                            
+        
+-- searchMin f Nothing ps = do
+--   (p', ps') <- pop ps;
+--   searchMin f (f Nothing p') ps'
+-- searchMin f (Just b) ps = maybe (Just b) Just $ do
+--   (p', ps') <- pop ps;
+--   searchMin f (f (Just b) p') ps'
 
 toList :: (Foldable m) => m a -> [a]
 toList = foldr (:) []
